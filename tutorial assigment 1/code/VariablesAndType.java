444a0157512bf51588dba92a8344ff52759921ce
Anish Sah <anishsah197@gmail.com>
2026-06-03 23:11:36 +0545

    Add files via upload
    make different files in same folder

---
+++ tutorial assigment 1/code/VariablesAndType.java
@@ -0,0 +1,15 @@
+public class VariablesAndTypes {
+    public static void main(String[] args) {
+        int myInt = 100;
+        double myDouble = 25.55;
+        boolean myBoolean = true;
+        char myChar = 'j';
+        String myString = "Learning Java";
+
+        System.out.println("int value: " + myInt + 
+                           "\ndouble value: " + myDouble + 
+                           "\nboolean value: " + myBoolean + 
+                           "\nchar value: " + myChar + 
+                           "\nstring value: " + myString);
+    }
+}
